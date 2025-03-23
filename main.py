5a38cdbf01cb67c86e0ccf1efb2f5cf58fb4e0c0
Felix Umland <futc@tuta.io>
2025-03-23 12:50:42 +0100

    first working prototype

---
+++ main.py
@@ -0,0 +1,366 @@
+import os
+
+from openai import OpenAI
+import os
+import json
+import xml.etree.ElementTree as ET
+
+OpenAI.api_key = os.environ["OPENAI_API_KEY"]
+client = OpenAI()
+# completion = client.chat.completions.create(
+#     model="gpt-4o",
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "Write a one-sentence bedtime story about a unicorn."
+#         }
+#     ]
+# )
+#
+# print(completion.choices[0].message.content)
+
+response = client.responses.create(
+    model="gpt-4o",
+    input=[
+        {"role": "system",
+         "content": "You are an award winning photographer, specializing in image editing and manipulation, especially in Adobe Lightroom."
+                    "Create settings for a Lightroom preset that matches the named theme."},
+        {"role": "user", "content": "Please make a lightroom preset that matches the theme of Vintage Summer."},
+    ],
+    text={
+        "format": {
+            "type": "json_schema",
+            "name": "camera_raw_settings",
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "Contrast2012": {
+                        "type": "integer"
+                    },
+                    "Highlights2012": {
+                        "type": "integer"
+                    },
+                    "Shadows2012": {
+                        "type": "integer"
+                    },
+                    "Whites2012": {
+                        "type": "integer"
+                    },
+                    "Blacks2012": {
+                        "type": "integer"
+                    },
+                    "Texture": {
+                        "type": "integer"
+                    },
+                    "Clarity2012": {
+                        "type": "integer"
+                    },
+                    "Dehaze": {
+                        "type": "integer"
+                    },
+                    "Vibrance": {
+                        "type": "integer"
+                    },
+                    "Saturation": {
+                        "type": "integer"
+                    },
+                    "ParametricShadows": {
+                        "type": "integer"
+                    },
+                    "ParametricDarks": {
+                        "type": "integer"
+                    },
+                    "ParametricLights": {
+                        "type": "integer"
+                    },
+                    "ParametricHighlights": {
+                        "type": "integer"
+                    },
+                    "ParametricShadowSplit": {
+                        "type": "integer"
+                    },
+                    "ParametricMidtoneSplit": {
+                        "type": "integer"
+                    },
+                    "ParametricHighlightSplit": {
+                        "type": "integer"
+                    },
+                    "Sharpness": {
+                        "type": "integer"
+                    },
+                    "SharpenRadius": {
+                        "type": "number"
+                    },
+                    "SharpenDetail": {
+                        "type": "integer"
+                    },
+                    "SharpenEdgeMasking": {
+                        "type": "integer"
+                    },
+                    "LuminanceSmoothing": {
+                        "type": "integer"
+                    },
+                    "ColorNoiseReduction": {
+                        "type": "integer"
+                    },
+                    "ColorNoiseReductionDetail": {
+                        "type": "integer"
+                    },
+                    "ColorNoiseReductionSmoothness": {
+                        "type": "integer"
+                    },
+                    "HueAdjustmentRed": {
+                        "type": "integer"
+                    },
+                    "HueAdjustmentOrange": {
+                        "type": "integer"
+                    },
+                    "HueAdjustmentYellow": {
+                        "type": "integer"
+                    },
+                    "HueAdjustmentGreen": {
+                        "type": "integer"
+                    },
+                    "HueAdjustmentAqua": {
+                        "type": "integer"
+                    },
+                    "HueAdjustmentBlue": {
+                        "type": "integer"
+                    },
+                    "HueAdjustmentPurple": {
+                        "type": "integer"
+                    },
+                    "HueAdjustmentMagenta": {
+                        "type": "integer"
+                    },
+                    "SaturationAdjustmentRed": {
+                        "type": "integer"
+                    },
+                    "SaturationAdjustmentOrange": {
+                        "type": "integer"
+                    },
+                    "SaturationAdjustmentYellow": {
+                        "type": "integer"
+                    },
+                    "SaturationAdjustmentGreen": {
+                        "type": "integer"
+                    },
+                    "SaturationAdjustmentAqua": {
+                        "type": "integer"
+                    },
+                    "SaturationAdjustmentBlue": {
+                        "type": "integer"
+                    },
+                    "SaturationAdjustmentPurple": {
+                        "type": "integer"
+                    },
+                    "SaturationAdjustmentMagenta": {
+                        "type": "integer"
+                    },
+                    "LuminanceAdjustmentRed": {
+                        "type": "integer"
+                    },
+                    "LuminanceAdjustmentOrange": {
+                        "type": "integer"
+                    },
+                    "LuminanceAdjustmentYellow": {
+                        "type": "integer"
+                    },
+                    "LuminanceAdjustmentGreen": {
+                        "type": "integer"
+                    },
+                    "LuminanceAdjustmentAqua": {
+                        "type": "integer"
+                    },
+                    "LuminanceAdjustmentBlue": {
+                        "type": "integer"
+                    },
+                    "LuminanceAdjustmentPurple": {
+                        "type": "integer"
+                    },
+                    "LuminanceAdjustmentMagenta": {
+                        "type": "integer"
+                    },
+                    "SplitToningShadowHue": {
+                        "type": "integer"
+                    },
+                    "SplitToningShadowSaturation": {
+                        "type": "integer"
+                    },
+                    "SplitToningHighlightHue": {
+                        "type": "integer"
+                    },
+                    "SplitToningHighlightSaturation": {
+                        "type": "integer"
+                    },
+                    "SplitToningBalance": {
+                        "type": "integer"
+                    },
+                    "ColorGradeMidtoneHue": {
+                        "type": "integer"
+                    },
+                    "ColorGradeMidtoneSat": {
+                        "type": "integer"
+                    },
+                    "ColorGradeShadowLum": {
+                        "type": "integer"
+                    },
+                    "ColorGradeMidtoneLum": {
+                        "type": "integer"
+                    },
+                    "ColorGradeHighlightLum": {
+                        "type": "integer"
+                    },
+                    "ColorGradeBlending": {
+                        "type": "integer"
+                    },
+                    "ColorGradeGlobalHue": {
+                        "type": "integer"
+                    },
+                    "ColorGradeGlobalSat": {
+                        "type": "integer"
+                    },
+                    "ColorGradeGlobalLum": {
+                        "type": "integer"
+                    },
+                    "GrainAmount": {
+                        "type": "integer"
+                    },
+                    "GrainSize": {
+                        "type": "integer"
+                    },
+                    "GrainFrequency": {
+                        "type": "integer"
+                    },
+                    "PostCropVignetteAmount": {
+                        "type": "integer"
+                    },
+                    "ShadowTint": {
+                        "type": "integer"
+                    },
+                    "RedHue": {
+                        "type": "integer"
+                    },
+                    "RedSaturation": {
+                        "type": "integer"
+                    },
+                    "GreenHue": {
+                        "type": "integer"
+                    },
+                    "GreenSaturation": {
+                        "type": "integer"
+                    },
+                    "BlueHue": {
+                        "type": "integer"
+                    },
+                    "BlueSaturation": {
+                        "type": "integer"
+                    },
+                    "CurveRefineSaturation": {
+                        "type": "integer"
+                    },
+                    "OverrideLookVignette": {
+                        "type": "boolean"
+                    },
+                    "ToneCurveName2012": {
+                        "type": "string"
+                    },
+                    "HasSettings": {
+                        "type": "boolean"
+                    },
+                    "Name": {
+                        "type": "string"
+                    },
+                    "ToneCurvePV2012": {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        }
+                    },
+                    "ToneCurvePV2012Red": {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        }
+                    },
+                    "ToneCurvePV2012Green": {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        }
+                    },
+                    "ToneCurvePV2012Blue": {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        }
+                    },
+                    "PointColors": {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        }
+                    }
+                },
+                "required": ["Contrast2012", "Highlights2012", "Shadows2012", "Whites2012", "Blacks2012", "Texture", "Clarity2012", "Dehaze", "Vibrance", "Saturation", "ParametricShadows", "ParametricDarks", "ParametricLights", "ParametricHighlights", "ParametricShadowSplit", "ParametricMidtoneSplit", "ParametricHighlightSplit", "Sharpness", "SharpenRadius", "SharpenDetail", "SharpenEdgeMasking", "LuminanceSmoothing", "ColorNoiseReduction", "ColorNoiseReductionDetail", "ColorNoiseReductionSmoothness", "HueAdjustmentRed", "HueAdjustmentOrange", "HueAdjustmentYellow", "HueAdjustmentGreen", "HueAdjustmentAqua", "HueAdjustmentBlue", "HueAdjustmentPurple", "HueAdjustmentMagenta", "SaturationAdjustmentRed", "SaturationAdjustmentOrange", "SaturationAdjustmentYellow", "SaturationAdjustmentGreen", "SaturationAdjustmentAqua", "SaturationAdjustmentBlue", "SaturationAdjustmentPurple", "SaturationAdjustmentMagenta", "LuminanceAdjustmentRed", "LuminanceAdjustmentOrange", "LuminanceAdjustmentYellow", "LuminanceAdjustmentGreen", "LuminanceAdjustmentAqua", "LuminanceAdjustmentBlue", "LuminanceAdjustmentPurple", "LuminanceAdjustmentMagenta", "SplitToningShadowHue", "SplitToningShadowSaturation", "SplitToningHighlightHue", "SplitToningHighlightSaturation", "SplitToningBalance", "ColorGradeMidtoneHue", "ColorGradeMidtoneSat", "ColorGradeShadowLum", "ColorGradeMidtoneLum", "ColorGradeHighlightLum", "ColorGradeBlending", "ColorGradeGlobalHue", "ColorGradeGlobalSat", "ColorGradeGlobalLum", "GrainAmount", "GrainSize", "GrainFrequency", "PostCropVignetteAmount", "ShadowTint", "RedHue", "RedSaturation", "GreenHue", "GreenSaturation", "BlueHue", "BlueSaturation", "CurveRefineSaturation", "OverrideLookVignette", "ToneCurveName2012", "HasSettings", "Name", "ToneCurvePV2012", "ToneCurvePV2012Red", "ToneCurvePV2012Green", "ToneCurvePV2012Blue", "PointColors"],
+                "additionalProperties": False
+            },
+            "strict": True
+        }
+    }
+)
+
+event = json.loads(response.output_text)
+print(event)
+print(response.usage.total_tokens)
+
+# Create the root element
+xmpmeta = ET.Element('x:xmpmeta', {
+    'xmlns:x': 'adobe:ns:meta/',
+    'x:xmptk': 'Adobe XMP Core 7.0-c000 1.000000, 0000/00/00-00:00:00'
+})
+
+rdf = ET.SubElement(xmpmeta, 'rdf:RDF', {
+    'xmlns:rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+})
+
+description = ET.SubElement(rdf, 'rdf:Description', {
+    'rdf:about': '',
+    'xmlns:crs': 'http://ns.adobe.com/camera-raw-settings/1.0/',
+    'crs:PresetType': 'Normal',
+    'crs:Cluster': '',
+    'crs:UUID': 'CCCAF6F2931240FAA798497F61D7C963',
+    'crs:SupportsAmount2': 'True',
+    'crs:SupportsAmount': 'True',
+    'crs:SupportsColor': 'True',
+    'crs:SupportsMonochrome': 'True',
+    'crs:SupportsHighDynamicRange': 'True',
+    'crs:SupportsNormalDynamicRange': 'True',
+    'crs:SupportsSceneReferred': 'True',
+    'crs:SupportsOutputReferred': 'True',
+    'crs:RequiresRGBTables': 'False',
+    'crs:CameraModelRestriction': '',
+    'crs:Copyright': '',
+    'crs:ContactInfo': '',
+    'crs:Version': '17.2',
+    'crs:ProcessVersion': '15.4',
+    'crs:ToneCurveName2012': 'Custom',
+    'crs:HasSettings': 'True',
+})
+
+# Update description with values from API response
+for key, value in event.items():
+    if isinstance(value, list):
+        parent_element = ET.SubElement(description, f'crs:{key}')
+        seq = ET.SubElement(parent_element, 'rdf:Seq')
+        for item in value:
+            li = ET.SubElement(seq, 'rdf:li')
+            li.text = str(item)
+    else:
+        description.set(f'crs:{key}', str(value))
+
+# Write the XMP to a file
+tree = ET.ElementTree(xmpmeta)
+tree.write(event['Name'] + '.xmp', encoding='utf-8', xml_declaration=True)
+
+print("XMP file created successfully.")
