3545276825c134f6860f4bdcc4439127244cfbee
Niket Kapoor <niket.kapoor@outlook.de>
2025-05-14 02:41:15 +0200

    upsi

--- few_shot_examples_fetcher.py
+++ few_shot_examples_fetcher.py
@@ -4,17 +4,18 @@ import pandas as pd
 import faiss
 import os
 
-def few_shot_examples(theme):
+model = SentenceTransformer("all-MiniLM-L6-v2")
+index = faiss.read_index(r"C:\Users\niket\OneDrive\Data Science\4. Semester\Cloud Computing and Distributed Systems\preset-generator\full_index_faiss.index")
+df = pd.read_csv(r"C:\Users\niket\OneDrive\Data Science\4. Semester\Cloud Computing and Distributed Systems\preset-generator\Themes Dataset\lightroom_theme_configs.csv")
 
-    path = r"C:\Users\niket\OneDrive\Data Science\4. Semester\Cloud Computing and Distributed Systems\preset-generator\Themes Dataset\lightroom_theme_configs.csv"
-    df = pd.read_csv(path)
-    model = SentenceTransformer("all-MiniLM-L6-v2")
+def few_shot_examples(theme, k=2):
+    if not theme or not isinstance(theme, str):
+        raise ValueError("Theme must be a non-empty string.")
 
-    index = faiss.read_index(r"C:\Users\niket\OneDrive\Data Science\4. Semester\Cloud Computing and Distributed Systems\preset-generator\full_index_faiss.index")
     query = model.encode([theme])
+    distances, indices = index.search(query, k=k)
+    top_indices = indices[0]
 
-    distances, indices = index.search(query, k=2)
-    top_indices = indices[0]  
     few_shot_themes = df["theme"].iloc[top_indices].tolist()
     few_shot_configs = df["config"].iloc[top_indices].tolist()
 
